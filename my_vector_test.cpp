56d0657d4e8d11af9560de19d5aae1377517ee75
Chai-YD <1030333788@qq.com>
2018-07-21 10:20:03 -0400

    vector的使用

---
+++ my_vector_test.cpp
@@ -0,0 +1,23 @@
+/***************************************************
+ * 对 vector的初步认识
+ * ************************************************/
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+////vector类中的[]不提供越界检查的功能，介意使用at（）方法，
+////因为它提供越界检查的功能
+//int  main(){
+//    vector<int> v1(10);
+//    int i = 0;;
+//    for(;i<10;i++){
+//        v1.push_back(i);
+//    }
+//    cout<<v1.back()<<endl;
+//    cout<<v1.front()<<endl;
+//    cout<<v1.at(12)<<endl;
+//    for(i = 0;i<10;i++){
+//        cout<<v1[i]<<" ";
+//    }
+//    cout<<endl;
+//    return 0;
+//}
