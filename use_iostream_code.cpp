a29a96521608a4af2a0111e867bbffa72adc5f63
Chai-YD <1030333788@qq.com>
2018-08-10 22:23:13 -0400

    输出流

--- use_iostream_code.cpp
+++ use_iostream_code.cpp
@@ -40,12 +40,92 @@
 //}
 
 
-//通过write成员函数进行无格式的输出
-#include<iostream>
-using std::cout;
+////通过write成员函数进行无格式的输出
+//#include<iostream>
+//using std::cout;
+//
+//int main(){
+//    char buf[] = "HAPPY BIRTHDAY";
+//    cout.write(buf,10);
+//}
 
+
+////标准输入示例程序
+//#include<iostream>
+//using std::cout;
+//using std::endl;
+//using std::cin;
+//
+//int main(){
+//    int grade,highestGrade = -1;
+//    cout<<"Enter grade(enter end-of-file to end):";
+//    while(cin>>grade){
+//        if(grade > highestGrade){
+//            highestGrade = grade;
+//        }
+//        cout<<"Enter grade(enter end-of-file to end):";
+//    }
+//    cout<<"\n highestGrade is :"<<highestGrade<<endl;
+//    return 0;
+//}
+
+
+////通过get和getline成员函数进行字符和字符串的输入
+//#include<iostream>
+//#include<cstdio>
+//using namespace std;
+//
+//int main(){
+//    char ch;
+//    while((ch = cin.get()) != EOF){
+//        cout<<ch;
+//    }
+//    return 0;
+//}
+
+
+// #include<iostream>
+// using namespace std;
+// 
+// int main(){
+//     string str;
+//     //getline(cin,str);
+//     //cout<<str;
+//     //cout<<str.size()<<endl;
+//     char ch;
+//     cin.get(ch);
+//     cout<<ch;
+//     return 0; 
+// }
+
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// 
+// int main(){
+//     char ch[20];
+//     cin.get(ch[0]);
+//     int i = 0;
+//     for(;i < 20 && ch[i] != '\n';++i){
+//         cin.get(ch[i + 1]);
+//     }
+//     cout<<i<<endl;
+//     return 0;
+// }
+
+
+//通过read函数进行无格式的输入
+#include<iostream>
+using namespace std;
 int main(){
-    char buf[] = "HAPPY BIRTHDAY";
-    cout.write(buf,10);
+    char buf[80];
+    cin.read(buf,20);
+
+    cout.write(buf,cin.gcount());
+    cout<<endl;
+    cout<<"总数："<<cin.gcount()<<endl;
+    return 0;
 }
+
 
