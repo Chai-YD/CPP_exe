34b1a37a12a93468c41ddb951f5e7bb3a96db4d4
Chai-YD <1030333788@qq.com>
2018-08-20 12:03:16 -0400

    行缓冲

---
+++ my_huanchong_test.c
@@ -0,0 +1,28 @@
+//#include<stdio.h>
+//#include<unistd.h>
+//int main(){
+//    printf("chaiyandong\n");
+//    sleep(3);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//#include<unistd.h>
+//int main(){
+//    printf("chaiyandong");
+//    sleep(3);
+//    return 0;
+//}
+
+#include<stdio.h>
+#include<unistd.h>
+int main(){
+    printf("chaiyandong");
+    fflush(stdout);
+    sleep(3);
+    return 0;
+}
+
+
+
+
